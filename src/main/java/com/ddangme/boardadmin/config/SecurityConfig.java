0665be9bfc78266f6edaeab75cc4ae2579cc8be3
ddangme <ssilver121@naver.com>
2024-02-08 07:36:07 +0900

    docs: 로그인 페이지 및 기초 OAuth 2.0 인증 옵션 추가
    #17
    타임리프 태그 sec:authorize를 활용하여 인증 상태에 따라 로그인, 로그아웃 버튼이 노출되도록 설정

--- src/main/java/com/ddangme/boardadmin/config/SecurityConfig.java
+++ src/main/java/com/ddangme/boardadmin/config/SecurityConfig.java
@@ -6,14 +6,17 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.*;
+
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .formLogin(Customizer.withDefaults())
+                .formLogin(withDefaults())
                 .logout(logout -> logout.logoutSuccessUrl("/"))
+                .oauth2Login(withDefaults())
                 .build();
     }
 
