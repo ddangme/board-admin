17881c914e73a70758f124d34d6571f419624495
ddangme <ssilver121@naver.com>
2024-02-16 17:43:17 +0900

    test: 어드민 회원 컨트롤러에 필요한 핸들러 메소드 및 테스트
    #27

--- src/main/java/com/ddangme/boardadmin/controller/AdminAccountController.java
+++ src/main/java/com/ddangme/boardadmin/controller/AdminAccountController.java
@@ -1,14 +1,10 @@
 package com.ddangme.boardadmin.controller;
 
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RequestMapping("/admin/members")
 @Controller
@@ -17,6 +13,13 @@ public class AdminAccountController {
     @GetMapping
     public String members(Model model) {
         return "admin/members";
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseBody
+    @DeleteMapping("/api/admin/members/{userId}")
+    public void delete(@PathVariable String userId) {
+
     }
 
 }
