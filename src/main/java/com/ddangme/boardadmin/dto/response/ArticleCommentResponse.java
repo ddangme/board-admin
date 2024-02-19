45133900948fa7e66757b213a6aba6ca7690f246
ddangme <ssilver121@naver.com>
2024-02-20 08:59:49 +0900

    feat: 댓글 관리 페이지 구현: 컨트롤러, 서비스, 뷰
    #35

---
+++ src/main/java/com/ddangme/boardadmin/dto/response/ArticleCommentResponse.java
@@ -0,0 +1,24 @@
+package com.ddangme.boardadmin.dto.response;
+
+import com.ddangme.boardadmin.dto.ArticleCommentDto;
+import com.ddangme.boardadmin.dto.UserAccountDto;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.time.LocalDateTime;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record ArticleCommentResponse(
+        Long id,
+        UserAccountDto userAccount,
+        String content,
+        LocalDateTime createdAt
+) {
+
+    public static ArticleCommentResponse of(Long id, UserAccountDto userAccount, String content, LocalDateTime createdAt) {
+        return new ArticleCommentResponse(id, userAccount, content, createdAt);
+    }
+
+    public static ArticleCommentResponse of(ArticleCommentDto dto) {
+        return ArticleCommentResponse.of(dto.id(), dto.userAccount(), dto.content(), dto.createdAt());
+    }
+}
